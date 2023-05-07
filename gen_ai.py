48e5cf3ce35991f7bc5da5a08e6b2adf8608ee3b
Ganiu Oluwasegun Aina <89828936+Ainaganiu@users.noreply.github.com>
2023-05-07 13:02:28 +0100

    Update gen_ai.py

--- gen_ai.py
+++ gen_ai.py
@@ -1,27 +1,34 @@
+import streamlit as st
 import cohere
+
+# Create a Cohere client with your API key
 co = cohere.Client('gh7zeWIg1ZDBOGjYP09jnBDpUoHpo7Q4GEZwQ2zq')
-def write_topic(topic):
-  topic = str(topic)
-  response = co.generate(
-    model='command-xlarge-nightly',
-    prompt= f'Create a SEO blogpost with Table of Contents for the \"{topic}"\:',
-    max_tokens=2130,
-    temperature=0.9,
-    k=0,
-    stop_sequences=[],
-    return_likelihoods='NONE')
-  return print('Prediction: {}'.format(response.generations[0].text))
 
+# Define a Streamlit app
+def app():
+    # Set the app title
+    st.title("AI Generated Blog Post")
 
-write_topic('animal')
+    # Create a text input for the user to enter their topic
+    topic = st.text_input("Enter topic for blog post")
 
-st.title("🚀 Startup Idea Generator")
+    # Create a button that the user can click to generate the blog post
+    if st.button("Generate Blog Post"):
+        # Call the Cohere API to generate the blog post
+        response = co.generate(
+            model='command-xlarge-nightly',
+            prompt=f'Create a SEO blogpost with Table of Contents for the \"{topic}"\:',
+            max_tokens=2130,
+            temperature=0.9,
+            k=0,
+            stop_sequences=[],
+            return_likelihoods='NONE'
+        )
+        
+        # Get the generated text from the API response and display it to the user
+        generated_text = response.generations[0].text
+        st.write(generated_text)
 
-form = st.form(key="user_settings")
-with form:
-    industry_input = st.text_input("Topic", key = "topic_input")
-    generate_button = form.form_submit_button("Generate Idea")
-    if generate_button:
-        startup_idea = generate_idea(industry_input)
-        st.markdown("##### " + startup_name)
-        st.write(startup_idea)
+# Run the app
+if __name__ == "__main__":
+    app()
